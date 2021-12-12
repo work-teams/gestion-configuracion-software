14401131d489f5b069d70b7cd7c0f6ce99ef7d3e
FrediCaballero <fredi.caballero@unmsm.edu.pe>
2021-12-11 22:50:34 -0500

    implementación de backend scrips

---
+++ 02 Desarrollo/SMPO/Desarrollo/SMPO-WEB/php/scripts.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" type="text/css" href="js/alertifyjs/css/themes/default.css">
+<link rel="stylesheet" type="text/css" href="js/alertifyjs/css/alertify.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/alertifyjs/alertify.js"></script>
