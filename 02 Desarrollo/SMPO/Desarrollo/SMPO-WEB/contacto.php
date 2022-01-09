565c18b3066097c91c08a4b5f3fde31057e273d5
WilliamsEduardo12 <williams.cachique@unmsm.edu.pe>
2022-01-09 17:43:21 -0500

    Contacto - Fondo Mejoras

--- 02 Desarrollo/SMPO/Desarrollo/SMPO-WEB/contacto.php
+++ 02 Desarrollo/SMPO/Desarrollo/SMPO-WEB/contacto.php
@@ -23,49 +23,85 @@
   
 </head>
 
-<body class="fondo">
+<!--Añadiendo el Fondo-->
+<body class="fondo"> 
 
 <nav class="navbar pb-1 pt-1 navbar-expand-lg navbar-dark bg-dark mb-2 text-center">
     <a href="Principal.php" class="navbar-brand text-white text-center mr-3 px-3">SmartMoney</a>
+    
     <button class= "navbar-toggler" data-target="#menu" data-toggle="collapse" type="button">
+    
       <span class="navbar-toggler-icon"></span>
+    
     </button>
+    
     <div class="collapse navbar-collapse" id="menu">
+      
       <ul class="navbar-nav mr-auto">
-      	<li class="nav-item active mx-5 px-3">
-      		<a href="invertir.php" class="nav-link" style="font-size:18px;">Invertir</a>
-        </li>
+      	
         <li class="nav-item active mx-5 px-3">
-      		<a href="grafico.php" class="nav-link " style="font-size:18px;">Gráficos</a>
-      	</li>
-      	<li class="nav-item active" style="">
-      		<a href="usuario.php" class="nav-link mx-5 px-3" style="font-size:18px;">Usuario</a>
-      	</li>
+      	
+          <a href="invertir.php" class="nav-link" style="font-size:18px;">Invertir</a>
+        
+        </li>
+        
+        <li class="nav-item active mx-5 px-3">
+      	
+          <a href="grafico.php" class="nav-link " style="font-size:18px;">Gráficos</a>
+      	
+        </li>
+      	
+        <li class="nav-item active" style="">
+      	
+          <a href="usuario.php" class="nav-link mx-5 px-3" style="font-size:18px;">Usuario</a>
+      	
+        </li>
+      
       </ul>
+      
       <div class="text-center">
+        
         <li class="navbar-text">
+         
           <a href="php/salir.php" style="font-size:18px;" class="nav-link">Cerrar sesión</a>
+        
         </li>	
+      
       </div>	
+    
     </div>
 </nav>
 
 <div class="container mt-4">
+      
       <div class="row">
-          <div class="col-md-4"></div>
+      
+        <div class="col-md-4"></div>
+        
           <div class="col-md-4">
+          
             <h3 class="text-center rounded text-light py-2 invertir bg-warning">Contáctanos</h3>
+          
           </div>
+          
           <div class="col-md-4"></div>
+        
         </div>
+      
       </div>
 
       <div class="container-fluid mt-2">
+        
         <div class="row">
+          
           <div class="col-12 text-center text-light col-md-12">
+          
             <h2 class="informacionc">Si tienes dudas con tu experiencia en Smart Money tienes a tu disposición los siguientes canales para enviarnos tus consultas.</h2>
+          
           </div>
+        
         </div>
+      
       </div>
 
       <div class="container mt-4">
