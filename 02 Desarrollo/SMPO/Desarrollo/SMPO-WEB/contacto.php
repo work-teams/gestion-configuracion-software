3ed6a5239cd461fdafc07977f079f8cc053a9827
WilliamsEduardo12 <williams.cachique@unmsm.edu.pe>
2022-01-09 17:50:07 -0500

    Optimizando Formato Fondo - Contacto

--- 02 Desarrollo/SMPO/Desarrollo/SMPO-WEB/contacto.php
+++ 02 Desarrollo/SMPO/Desarrollo/SMPO-WEB/contacto.php
@@ -105,64 +105,97 @@
       </div>
 
       <div class="container mt-4">
+        
         <div class="row">
 
           <div class="col"></div>
+          
           <div class="col-md-4 text-center">
+            
             <a href="https://wa.me/51979204403" target="_blank"><img src="img/whatsappimg.png" class="btnw img-fluid" alt="whatsapp" srcset=""></a>
             <p class="text-light infot m-2 informacionc">Llámanos o contáctanos al Whatsapp.</p>
+          
           </div>
 
           <div class="col-md-4 text-center">
+          
             <a href="#" type="button" target="" data-toggle="modal" data-target="#exampleModal" data-whatever="SmartMoney"><img src="img/gmailok.png" class="btnw img-fluid" alt="whatsapp" srcset=""></a>
             <p class="text-light m-2 infot informacionc">Envíanos un correo electrónico.</p>
+          
           </div>
+          
           <div class="col"></div>
 
         </div>
 
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        
         <div class="modal-dialog" role="document">
+          
           <div class="modal-content">
+            
             <div class="modal-header">
+              
               <h5 class="modal-title" id="exampleModalLabel">Nuevo mensaje</h5>
+              
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              
                 <span aria-hidden="true">&times;</span>
+              
               </button>
+            
             </div>
 
             <div class="modal-body">
+              
               <form>
 
                 <div class="form-group">
+                  
                   <label for="recipient-name" class="col-form-label">Asunto:</label>
                   <input type="text" class="form-control" id="recipient-name">
+                
                 </div>
 
                 <div class="form-group">
+                
                   <label for="message-text" class="col-form-label">Mensaje:</label>
                   <textarea class="form-control" id="message-text"></textarea>
+                
                 </div>
 
               </form>
+            
             </div>
 
             <div class="modal-footer">
+              
               <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+              
               <button type="button" class="btn btn-success" data-dismiss="modal">Enviar</button>
+            
             </div>
 
           </div>
+        
         </div>
+      
       </div>
+      
       </div>
   </div>
+  
   <script src="funcionesJS/contacto.js"></script>
+
 </body>
+
 </html>
 
 <?php
+
 } else {
-	header("location:index.php");
-	}
+	
+  header("location:index.php");
+	
+}
 ?>
