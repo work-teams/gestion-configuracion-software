70c4ed2279cccbb03b04f097e2b19068198f85c6
WilliamsEduardo12 <williams.cachique@unmsm.edu.pe>
2022-01-09 20:06:29 -0500

    Criptomoneda Mejora

--- 02 Desarrollo/SMPO/Desarrollo/SMPO-WEB/criptomoneda.php
+++ 02 Desarrollo/SMPO/Desarrollo/SMPO-WEB/criptomoneda.php
@@ -21,72 +21,132 @@
 </head>
 
 <body class="fondo" onload="cargar()" >
+
 <nav class="navbar pb-1 pt-1 navbar-expand-lg navbar-dark bg-dark mb-2 text-center">
+    
     <a href="Principal.php" class="navbar-brand text-white text-center mr-3 px-3">SmartMoney</a>
+    
     <button class= "navbar-toggler" data-target="#menu" data-toggle="collapse" type="button">
-      <span class="navbar-toggler-icon"></span>
+     
+        <span class="navbar-toggler-icon"></span>
+    
     </button>
+    
     <div class="collapse navbar-collapse" id="menu">
+      
       <ul class="navbar-nav mr-auto">
-      	<li class="nav-item active mx-5 px-3">
-      		<a href="invertir.php" class="nav-link" style="font-size:18px;">Invertir</a>
-        </li>
+      	
         <li class="nav-item active mx-5 px-3">
-      		<a href="grafico.php" class="nav-link " style="font-size:18px;">Gráficos</a>
-      	</li>
-      	<li class="nav-item active" style="">
-      		<a href="usuario.php" class="nav-link mx-5 px-3" style="font-size:18px;">Usuario</a>
-      	</li>
+      	
+          <a href="invertir.php" class="nav-link" style="font-size:18px;">Invertir</a>
+        
+        </li>
+        
+        <li class="nav-item active mx-5 px-3">
+      	
+          <a href="grafico.php" class="nav-link " style="font-size:18px;">Gráficos</a>
+      	
+        </li>
+      	
+        <li class="nav-item active" style="">
+      	
+          <a href="usuario.php" class="nav-link mx-5 px-3" style="font-size:18px;">Usuario</a>
+      	
+        </li>
+      
       </ul>
+      
       <div class="text-center">
+        
         <li class="navbar-text">
+        
           <a href="php/salir.php" style="font-size:18px;" class="nav-link">Cerrar sesión</a>
+        
         </li>	
+      
       </div>	
+    
     </div>
+
 </nav>
+  
   <div class="container-fluid">
+        
         <div>
+        
           <h2 class="text-center rounded text-light mt-3 py-2 bg-success">COMPRA Y VENTA DE CRIPTOMONEDAS</h2>
+        
         </div>
 
       <div class="row text-center">
+        
         <div class="col-lg-2 col-md-4 col-sm-6 py-2 px-1">
+        
           <div class="card">
-          <h3 class="card-title-text">BITCOIN</h3>
-          <img class="card-img-top" src="img/bitcoin2.jpg" alt="bitcoin" style="width:100%">
-          <div class="row">
-            <div class="card-body">
+          
+            <h3 class="card-title-text">BITCOIN</h3>
+            <img class="card-img-top" src="img/bitcoin2.jpg" alt="bitcoin" style="width:100%">
+          
+            <div class="row">
+              
+              <div class="card-body">
+                
+                <div class="">
+                
+                  <button type="input" class="btn btn-success btn-block my-1">
+                    
+                    <div aclass="trade-button-title" >COMPRAR: <span id ="ComprarBitcoin">0</span></div>
+              
+                  </button>
+              
+                </div>
               <div class="">
-                <button type="input" class="btn btn-success btn-block my-1">
-                  <div aclass="trade-button-title" >COMPRAR: <span id ="ComprarBitcoin">0</span></div>
-                </button>
-              </div>
-              <div class="">
+              
                 <button button type="input" class="btn btn-primary btn-block my-1">
+                
                   <div aclass="trade-button-title">VENDER: <span id ="VenderBitcoin">0</span></div>
+                
                 </button>
+              
               </div>
+            
             </div>
+          
           </div>
+        
         </div>
-        </div>
+        
+      </div>
+        
         <div class="col-lg-2 col-md-4 col-sm-6 py-2 px-1">
+          
           <div class="card">
-          <h3 class="card-title-text">ETHERUM</h3>
-          <img class="card-img-top" src="img/etherum.jpg" alt="etherum" style="width:100%">
+          
+            <h3 class="card-title-text">ETHERUM</h3>
+            <img class="card-img-top" src="img/etherum.jpg" alt="etherum" style="width:100%">
+          
           <div class="row">
+            
             <div class="card-body">
+            
               <button type="input" class="btn btn-success btn-block my-1">
               <div aclass="trade-button-title" >COMPRAR: <span id ="ComprarEtherum">0</span></div>
+            
             </button>
+            
             <button type="input" class="btn btn-primary btn-block my-1">
+            
               <div aclass="trade-button-title">VENDER: <span id ="VenderEtherum">0</span></div>
             </button>
+          
           </div>
-          </div>
+          
         </div>
-        </div>
+        
+      </div>
+        
+    </div>
+    
         <div class="col-lg-2 col-md-4 col-sm-6 py-2 px-1">
           <div class="card">
           <h2 class="card-title-text">RIPPLE</h2>
