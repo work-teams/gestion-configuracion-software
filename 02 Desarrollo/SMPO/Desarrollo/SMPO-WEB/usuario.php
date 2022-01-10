37214076ada0c49d3a8c876326d39aec3e3090f2
marceloJimenezD <64915545+marceloJimenezD@users.noreply.github.com>
2022-01-10 11:53:53 -0500

    mejorando usuario.php

--- 02 Desarrollo/SMPO/Desarrollo/SMPO-WEB/usuario.php
+++ 02 Desarrollo/SMPO/Desarrollo/SMPO-WEB/usuario.php
@@ -6,46 +6,83 @@
   $myusuario = strtoupper($myusuario);
   if(isset($_SESSION['user']))
   {
-    ?>
-      <!DOCTYPE html>
-      <html lang="en">
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Smart Money</title>
+	<?php require_once "php/scripts.php";?>
+	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/estilosComunes.css">
 
-      <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Smart Money</title>        
-        <link rel="stylesheet" href="css/estilosComunes.css">
-      </head>
+</head>
+<body class="fondo">
+<nav class="navbar pb-1 pt-1 navbar-expand-lg navbar-dark bg-dark mb-2 text-center">
+    <a href="Principal.php" class="navbar-brand text-white text-center mr-3 px-3">SmartMoney</a>
+    <button class= "navbar-toggler" data-target="#menu" data-toggle="collapse" type="button">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="menu">
+      <ul class="navbar-nav mr-auto">
+      	<li class="nav-item active mx-5 px-3">
+      		<a href="invertir.php" class="nav-link" style="font-size:18px;">Invertir</a>
+        </li>
+        <li class="nav-item active mx-5 px-3">
+      		<a href="grafico.php" class="nav-link " style="font-size:18px;">Gráficos</a>
+      	</li>
+      	<li class="nav-item active" style="">
+      		<a href="usuario.php" class="nav-link mx-5 px-3" style="font-size:18px;">Usuario</a>
+      	</li>
+      </ul>
+      <div class="text-center">
+        <li class="navbar-text">
+          <a href="php/salir.php" style="font-size:18px;" class="nav-link">Cerrar sesión</a>
+        </li>	
+      </div>	
+    </div>
+</nav>
+<div class="container-fluid">
 
-      <body>
-      <nav class="navbarzzz">
-        <ul>
-            <li><a href="Principal.php">SmartMoney</a></li>
-            <li><a href="invertir.php">Invertir</a></li>
-            <li><a href="grafico.php">Gráficos</a></li>
-            <li><a href="usuario.php">Usuario</a></li>
-            <li><a href="php/salir.php">Salir</a></li>
-        </ul>
-    </nav> 
-
-        <a class="user-name"><?php echo $myusuario ?></a>
-
-        <div class="user-btn">
-          <div class="user-box">
-            <a href="modificardatos.php">Datos<img src="img/datos_usuario.png"></a>
-            
+      <div class="row">
+        <h3 class="col-4"></h3>
+        <h3 class="text-center col-md-4 rounded text-light mx-3 py-0 invertir bg-primary mb-0"><?php echo $myusuario ?></h3>
+        <h3 class="col-4"></h3>
+      </div>
+      <div class="row">
+          <div class="col-2"></div>
+          <div class="col-8">
+            <div class="row">
+                <div class="col-md-6 col-12 con">
+                    <img src="img/datos.jpg" class="rounded img-fluid imgv" alt="" style="border: 1px solid black">
+                    <a class="btn botonEnlace" href="modificardatos.php">DATOS</a>
+                </div>
+                <div class="col-md-6 col-12 con">
+                    <img src="img/historial.jpg" class="rounded img-fluid imgv" alt="" style="border: 1px solid black">
+                    <a class="btn botonEnlace" href="historial.php">HISTORIAL</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-12 con">
+                    <img src="img/cupon.jpg" class="rounded img-fluid imgv" alt="" style="border: 1px solid black">
+                    <a class="btn botonEnlace" href="#">CUPÓN</a>
+                </div>
+                <div class="col-md-6 col-12 con">
+                    <img src="img/contacto.jpg" class="rounded img-fluid imgv" alt="" style="border: 1px solid black">
+                    <a class="btn botonEnlace" href="contacto.php">CONTACTO</a>
+                </div>
+            </div>
           </div>
-          <div class="user-box" >
-            <a href="historial.php" style="  margin-left: 105px;margin-right: 105px;">Historial<img src="img/historial_usuario.png"></a>
-            
-          </div>          
-        </div>
+          <div class="col-2"></div>
+      </div>
+  </div>
 
-      </body>
-      </html>
-    <?php
-  } else {
-    header("location:index.php");
-  }
+</body>
+</html>
+
+<?php
+} else {
+	header("location:index.php");
+	}
 ?>
